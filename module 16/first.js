41f8fbc86fb4ca4ac97ca3509f6572d9f00bc6c1
shahoraiar <shahoraiar2000@gmail.com>
2024-10-03 20:37:46 +0600

    module 16 start javaScript

---
+++ module 16/first.js
@@ -0,0 +1,44 @@
+console.log('the number is : ', 567)
+
+var a = 5+9;
+console.log('sum : ', a);
+console.log('sum : ', 10+5);
+
+var namee = "emon";
+console.log('print my name : ', namee)
+
+console.log(typeof namee)
+console.log(typeof a)
+
+var var_name;
+console.log(typeof var_name);
+
+var num = 0.25
+console.log(typeof num)
+
+var str1 = "my name is:"
+var str2 = "EMON"
+
+console.log(str1 + ' '+ str2);
+
+var str3 = '20';
+var num3 = parseInt(str3)
+
+console.log('num3 : ', num3)
+console.log('num3 type : ', typeof num3)
+
+var num4 = parseInt('500')
+var num5 = parseInt(600)
+
+console.log('num4 : ', num4)
+console.log('num4 type : ', typeof num4)
+console.log('num5 : ', num5)
+console.log('num5 type : ', typeof num5)
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second ;
+
+var new_total = (first+second).toFixed(4)
+console.log('total : ', total.toFixed(2))
+console.log('new total : ', new_total);
