60274020ff47ccaab56110d827d9c5b34d9058e5
shahoraiar <shahoraiar2000@gmail.com>
2024-10-09 16:15:13 +0600

    module 25_6 finished. addeventlistener

---
+++ Module 25/js/events.js
@@ -0,0 +1,11 @@
+console.log('events js connect')
+
+function makeYellow(){
+    document.body.style.backgroundColor = 'yellow';
+}
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+}
